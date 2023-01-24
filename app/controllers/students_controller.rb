b6350e832315e08e18236194eee220f96b42d50f
Jake Wehder <jw@email.comjake>
2023-01-24 13:13:10 -0600

    commit tree 52d0219f4a64c13b15ba6965159ab14450c227c8

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+
+    def index 
+        students = Student.all 
+        render json: students 
+    end
+
+    def grades
+        students_grades = Student.all.order(grade: :desc)
+        render json: students_grades
+    end
+
+    def highest_grade 
+        highest_grade = Student.all.order(grade: :desc).limit(1)
+        render json: highest_grade
+    end
+end
